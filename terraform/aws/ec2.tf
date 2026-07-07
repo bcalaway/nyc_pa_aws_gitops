baad546deea207a2761b984efbe40928bae6299a
bcalaway <calawayw@yahoo.com>
2026-07-06 20:59:17 -0400

    Extend Prometheus retention to 5 years, grow EC2 root volume to 200GB
    30d retention was too short for long-term trend analysis. Sized the
    volume/retention.size cap for ~5y of data across current + planned
    (Milestone 8) exporters with headroom for Loki/Docker/OS on the same disk.

--- terraform/aws/ec2.tf
+++ terraform/aws/ec2.tf
@@ -22,7 +22,7 @@ resource "aws_instance" "hub" {
   iam_instance_profile   = aws_iam_instance_profile.hub.name
 
   root_block_device {
-    volume_size           = 40
+    volume_size           = 200
     volume_type           = "gp3"
     delete_on_termination = true
     encrypted             = true
